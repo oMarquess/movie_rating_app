12454352c94a1e2ad5c3ed283046e1e1a905d0fe
oMarquess <pymasterclass@gmail.com>
2023-11-19 21:35:24 +0000

    problem solved

--- src/moviewatch/movie_watch_app/urls.py
+++ src/moviewatch/movie_watch_app/urls.py
@@ -2,15 +2,15 @@
 #from .views import MovieListView
 from django.contrib import admin
 from django.urls import path
-from . views import MovieCreateView, MovieListView
+from . views import MovieCreateView, MovieListView, MovieUpdateView, MovieCreateView, MovieDeleteView, MovieDetailView
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('list', MovieListView.as_view(), name='movie_list'),
-    # path('movies/<int:pk>/', MovieDetailView.as_view(), name='movie_detail'),
+    path('movies/<int:pk>/', MovieDetailView.as_view(), name='movie_detail'),
     path('add/', MovieCreateView.as_view(), name='movie_add'),
-    # path('movies/<int:pk>/edit/', MovieUpdateView.as_view(), name='movie_edit'),
-    # path('movies/<int:pk>/delete/', MovieDeleteView.as_view(), name='movie_delete'),
+    path('movies/<int:pk>/edit/', MovieUpdateView.as_view(), name='movie_edit'),
+    path('movies/<int:pk>/delete/', MovieDeleteView.as_view(), name='movie_delete'),
 
 ]
--- src/moviewatch/movie_watch_app/views.py
+++ src/moviewatch/movie_watch_app/views.py
@@ -24,23 +24,24 @@ class MovieListView(ListView):
     template_name = 'movie_list.html'
     context_object_name = 'movie'
 
-# class MovieCreateView(CreateView):
-#     model = Movie
-#     fields = ['title', 'description', 'year_released', 'watched']
-#     template_name = 'movie_form.html'
-#     success_url = reverse_lazy('movie_list')
+class MovieCreateView(CreateView):
+    model = Movie
+    fields = ['title', 'description', 'year_released', 'watched']
+    template_name = 'movie_form.html'
+    success_url = reverse_lazy('movie_list')
 
-# class MovieUpdateView(UpdateView):
-#     model = Movie
-#     fields = ['title', 'description', 'year_released', 'watched']
-#     template_name = 'movie_form.html'
-#     success_url = reverse_lazy('movie_list')
+class MovieUpdateView(UpdateView):
+    model = Movie
+    fields = ['title', 'description', 'year_released', 'watched']
+    template_name = 'movie_form.html'
+    success_url = reverse_lazy('movie_list')
 
-# class MovieDetailView(DetailView):
-#     model = Movie
-#     template_name = 'movie_detail.html'
+class MovieDetailView(DetailView):
+    model = Movie
+    template_name = 'movie_detail.html'
+    context_object_name = 'movie'
 
-# class MovieDeleteView(DeleteView):
-#     model = Movie
-#     template_name = 'movie_confirm_delete.html'
-#     success_url = reverse_lazy('movie_list')
+class MovieDeleteView(DeleteView):
+    model = Movie
+    template_name = 'movie_confirm_delete.html'
+    success_url = reverse_lazy('movie_list')
